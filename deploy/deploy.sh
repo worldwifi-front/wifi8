61cbf9cec045cccf31da99555916717f31aff3c0
Stanislaw Bogatkin <sbog@sbog.ru>
2018-02-04 12:21:09 +0300

    Add an ability to deploy to Heroku staging
    To be able to deploy frontend on push event, add deploy logic to
    autopush new data to Heroku staging.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,10 +9,11 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$BUILD_BRANCH" ];
     exit 0
 fi
 
-HEAD_COMMIT=`git rev-parse --verify --short HEAD`
-BANANA_REPO=`git config remote.origin.url`
-BANANA_SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-UPSTREAM_REPO="git@github.com:worldwifi-front/wifi8.git"
+FRONTEND_HEAD_COMMIT=`git rev-parse --verify --short HEAD`
+FRONTEND_REPO=`git config remote.origin.url`
+BACKEND_NAME="worldwifi-rails"
+BACKEND_REPO="git@github.com:oomag/${BACKEND_NAME}.git"
+HEROKU_REPO_URL="git@heroku.com:worldwifistaging.git"
 
 echo "Prepare the key..."
 # Encryption key is a key stored in travis itself
@@ -24,14 +25,38 @@ echo "Add decoded key to the ssh agent keystore"
 eval `ssh-agent -s`
 ssh-add $OUT_KEY
 
-echo "Add upstream repo"
-git add remote upstream $UPSTREAM_REPO
+echo "Pull upstream backend repo"
+pushd ..
+git clone $BACKEND_REPO $BACKEND_NAME
+echo "Return back to the original repo"
 popd
 
-echo "Add new data to the upstream repo"
-  git config user.name "Travis CI"
-  git config user.email "$COMMIT_AUTHOR_EMAIL"
+echo "Checkout to staging branch in backend repo"
+pushd ../$BACKEND_NAME
+git checkout staging
+popd
+
+echo "Copy data to the backend repo"
+mkdir -p ../$BACKEND_NAME/public/
+cp -rf ru ../$BACKEND_NAME/public/
+cp -rf en ../$BACKEND_NAME/public/
+
+echo "Add new data to the backend repo git"
+pushd ../$BACKEND_NAME
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git add -A .
+if ! [[ -z $(git status -s) ]] ; then
+  echo "Pushing changes to the $BACKEND_REPO master branch"
+  git commit -m "Add new build data from $BACKEND_NAME frontend $HEAD_COMMIT commit"
   git push origin master
+  echo "Add $HEROKU_REPO_URL as heroku remote"
+  git remote add heroku $HEROKU_REPO_URL
+  echo "Pushing to heroku remote..."
+  export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+  git push -q heroku staging:$BUILD_BRANCH
+  echo "All done."
 else
   echo "There are no changes in result build, so nothing to push forward. End here."
   exit 0
