c46135fa306d8fdd0cf9fc4148a229ec2aad299a
Anton_Palchikov <palchikov.comunity@yandex.ru>
2018-02-07 15:21:33 +0300

    Подписка

--- ru/mail.php
+++ ru/mail.php
@@ -3,13 +3,35 @@
 header('Access-Control-Allow-Origin: *');
 
 $headers = "Content-type: text/html; charset=utf-8 \r\n";
-$headers .= "From: worldwifi <info@worldwifi.io>\r\n"; // от кого
+$headers .= "From: worldwifi <support@notifications.worldwifi.io>\r\n"; // от кого
 
-
-$email = strip_tags($_POST['email']);
+$apikey = '584640f470d805483a6ee638484bfdd9-us17';
+$auth = base64_encode( 'user:'.$apikey );
 
 $subject = 'Подписка';
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
 
+$data = array(
+    'apikey'        => $apikey,
+    'email_address' => $email,
+    'status'        => 'subscribed',
+    'merge_fields'  => array(
+        'FNAME' => $name
+    )
+);
+$json_data = json_encode($data);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://us17.api.mailchimp.com/3.0/lists/675ec4fcb9/members');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+                                            'Authorization: Basic '.$auth));
+curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);                                                                                                                  
 
 
 if(!(preg_match("/[0-9a-z]+@[0-9a-z]/", $email))){
@@ -17,13 +39,22 @@ if(!(preg_match("/[0-9a-z]+@[0-9a-z]/", $email))){
     $text = 'Введи корректный E-mail';
 }else {
 
-	$mail = mail('info@worldwifi.io', $subject, $email, $headers);
-    if ($mail) {
-    	$status = 'success';
-    	$text = 'Вы успешно подписались на наши обновления';
+    $out = curl_exec($ch);
+
+    $result = json_decode($out);
+
+    if ($result->status == 'subscribed') {
+        $mail = mail($email, $subject, 'your success', $headers);
+        if ($mail) {
+            $status = 'success';
+            $text = 'Вы успешно подписались на наши обновления';
+        }else {
+            $status = 'error';
+            $text = 'Повторите попытку';
+        }
     }else {
-    	$status = 'error';
-    	$text = 'Повторите попытку';
+        $status = 'error';
+        $text = 'Этот email адрес уже используется';
     }
 }
 
