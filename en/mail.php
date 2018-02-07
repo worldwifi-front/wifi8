32d603c38993118c4dfcf585aecab84ae79ab097
Anton_Palchikov <palchikov.comunity@yandex.ru>
2018-02-07 15:21:33 +0300

    Подписка

--- en/mail.php
+++ en/mail.php
@@ -3,15 +3,36 @@
 header('Access-Control-Allow-Origin: *');
 
 $headers = "Content-type: text/html; charset=utf-8 \r\n";
-$headers .= "From: worldwifi <info@worldwifi.io>\r\n"; // от кого
+$headers .= "From: worldwifi <support@notifications.worldwifi.io>\r\n"; // от кого
 
-$key = '9a72c63639e0138472daf13888995607-us17';
-
-
-$email = strip_tags($_POST['email']);
+$apikey = '584640f470d805483a6ee638484bfdd9-us17';
+$auth = base64_encode( 'user:'.$apikey );
 
 $subject = 'Subscribe';
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
 
+
+$data = array(
+    'apikey'        => $apikey,
+    'email_address' => $email,
+    'status'        => 'subscribed',
+    'merge_fields'  => array(
+        'FNAME' => $name
+    )
+);
+$json_data = json_encode($data);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://us17.api.mailchimp.com/3.0/lists/675ec4fcb9/members');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+                                            'Authorization: Basic '.$auth));
+curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);                                                                                                                  
 
 
 if(!(preg_match("/[0-9a-z]+@[0-9a-z]/", $email))){
@@ -19,13 +40,22 @@ if(!(preg_match("/[0-9a-z]+@[0-9a-z]/", $email))){
     $text = 'Enter a valid E-mail';
 }else {
 
-	$mail = mail('info@worldwifi.io', $subject, $email, $headers);
-    if ($mail) {
-    	$status = 'success';
-    	$text = 'You have successfully subscribed to our updates';
+    $out = curl_exec($ch);
+
+    $result = json_decode($out);
+
+	if ($result->status == 'subscribed') {
+        $mail = mail($email, $subject, 'your success', $headers);
+        if ($mail) {
+            $status = 'success';
+            $text = 'You have successfully subscribed to our updates';
+        }else {
+            $status = 'error';
+            $text = 'Try again';
+        }
     }else {
-    	$status = 'error';
-    	$text = 'Try again';
+        $status = 'error';
+        $text = 'This email is already registered';
     }
 }
 
